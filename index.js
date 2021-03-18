eaa9e5d4ab766dcc6e37d525f88f5d14f2076ac8
Rick <dirklo@gmail.com>
2021-03-18 13:06:04 +0100

    commit tree 1dc8aeac3c9b206ac7a2b9e982492532b8d3b278

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+const mapToNegativize = function(array) {
+    return array.map((num) => -num)
+}
+const mapToNoChange = function(array) {
+    return array.map((num) => num)
+}
+const mapToDouble = function(array) {
+    return array.map((num) => num*2)
+}
+const mapToSquare = function(array) {
+    return array.map((num) => num**2)
+}
+const reduceToTotal = function(array, startingPoint = 0) {
+    return array.reduce((item, sum) => sum += item) + startingPoint
+}
+const reduceToAllTrue = function(array) {
+    return array.reduce(function(item, result = true) {
+        if (!!item && result) {
+            return result = true;
+        } else {
+            return result = false;
+        }
+    })
+}
+const reduceToAnyTrue = function(array) {
+    return array.reduce(function(item, result = false) {
+        if (!!item || result) {
+            return result = true;
+        } else {
+            return result = false;
+        }
+    })
+}
